a9fe32526f24427824a2f8678268a17751ee2750
Tom Biegeleisen <tom.biegeleisen@huffingtonpost.com>
2013-02-25 11:06:13 -0500

    added regular expression match support in URL

--- js/jquery.mockjson.js
+++ js/jquery.mockjson.js
@@ -1,137 +1,37 @@
-(function($) {
+/*global jQuery: false */
+;(function($) {
 
-var _mocked = [];
-$.mockJSON = function(request, template) {
-    for (var i = 0; i < _mocked.length; i++) {
-        var mock = _mocked[i];
-        if (mock.request.toString() == request.toString()) {
-            _mocked.splice(i, 1);
-            break;
-        }
-    }
+var _mocked = [],
+    _original_ajax = $.ajax,
+    _random_numbers = [0.021768910889510606,0.23762323165420307,0.9079616118204306,0.6534305309997466,0.22049697572443694,0.07687466163364898,0.8017428775547905,0.16165353264404825,0.5124345671670483,0.19337327636624613,0.39963994200698416,0.8012592654139514,0.22474962687229938,0.9791396234452399,0.7965428353317756,0.9777664340629622,0.5135216702983731,0.7407128236192145,0.12880984991420075,0.8186600800491484,0.5187691445438851,0.034723021925916586,0.5625092833040853,0.02502838571997701,0.663696305503698,0.3481608684353138,0.8991623585175106,0.3640542564277087,0.8320766874121723,0.012778915627689846,0.1427680370061336,0.9774408289203956,0.010229381207667587,0.2596610885223093,0.6150540104297127,0.7130773919030915,0.8638338302974085,0.6178483032907357,0.980312844391733,0.5007277415012348,0.6348672031113127,0.4400097775503303,0.8468458451408212,0.38724997893647317,0.690237920987028,0.19850102297146477,0.44895115941315766,0.22283381913760725,0.031228117310125314,0.3367510872581615,0.28155752394210787,0.14696694832580504,0.08164635161760991,0.8837733477785624,0.4590179148539142,0.9613195413217465,0.11263127577456922,0.743695635896287,0.0002424891439143373,0.1964622832546613,0.7333363138878922,0.5575568682003356,0.20426374168098604,0.18030934250338893,0.9792636408392759,0.30121911048336913,0.7734906886720265,0.6984051127767527,0.6638058511379343,0.3310956256388182,0.36632372827973203,0.8996494702333895,0.8235917663049763,0.418496734118911,0.8164648495097332,0.9457831606354686,0.2845227542117049,0.42374718399151545,0.3431728911657228,0.5289314006219973,0.6029243600407113,0.6528301140700757,0.6948768236197832,0.7887302784092911,0.8950274196119906,0.6121642239166305,0.31797481561514696,0.34903732589844216,0.3580320092281766,0.8312225728434115,0.32331010157206974,0.16395388672837796,0.6072960306003872,0.6580526967999424,0.23472961545632742,0.6138637855489343,0.3067303339060682,0.44935935129958315,0.24729465243280668,0.8244189715967711];
 
-    _mocked.push({
-        request:request,
-        template:template
-    });
-    
-    return $;
-};
+// This array stores pattern matches in the given URL
+var url_matches = [];
 
-$.mockJSON.random = true;
+// This array stores query string properties
+var query_string = {};
 
-
-var _original_ajax = $.ajax;
-$.ajax = function(options) {
-    if (options.dataType === 'json') {
-        for (var i = 0; i < _mocked.length; i++) {
-            var mock = _mocked[i];
-            if (mock.request.test(options.url)) {
-                options.success($.mockJSON.generateFromTemplate(mock.template));
-                return $;
-            }
-        }
-    }
-    
-    return _original_ajax.apply(this, arguments);
+// Returns an array of query string parameters for a given URL
+function parseQueryString(url) {
+	var qpos = url.indexOf("?");
+	if ( qpos === -1 ) {
+		return;
+	}
+	var pairs = url.substring(qpos + 1).split("&");
+	for ( var i in pairs ) {
+		var p = pairs[i].split("=");
+		query_string[p[0]] = p[1];
+	}
 }
 
-
-$.mockJSON.generateFromTemplate = function(template, name) {
-    var length = 0;
-    var matches = (name || '').match(/\w+\|(\d+)-(\d+)/);
-    if (matches) {
-        var length_min = parseInt(matches[1], 10);
-        var length_max = parseInt(matches[2], 10);
-        length = Math.round(rand() * (length_max - length_min)) + length_min;
-    }
-        
-    var generated = null;
-    switch (type(template)) {
-        case 'array':
-            generated = [];
-            for (var i = 0; i < length; i++) {
-                generated[i] = $.mockJSON.generateFromTemplate(template[0]);
-            }
-            break;
-
-        case 'object':
-            generated = {};
-            for (var p in template) {
-                generated[p.replace(/\|(\d+-\d+|\+\d+)/, '')] = $.mockJSON.generateFromTemplate(template[p], p);
-                var inc_matches = p.match(/\w+\|\+(\d+)/);
-                if (inc_matches && type(template[p]) == 'number') {
-                    var increment = parseInt(inc_matches[1], 10);
-                    template[p] += increment;
-                }
-            }
-            break;
-
-        case 'number':
-            generated = (matches)
-                ? length
-                : template;
-            break;
-
-        case 'boolean':
-            generated = (matches)
-                ? rand() >= 0.5
-                : template;
-            break;
-
-        case 'string':
-            if (template.length) {
-                generated = '';
-                length = length || 1;
-                for (var i = 0; i < length; i++) {
-                    generated += template;
-                }
-                var keys = generated.match(/@([A-Z_0-9\(\),]+)/g) || [];
-                for (var i = 0; i < keys.length; i++) {
-                    var key = keys[i];
-                    generated = generated.replace(key, getRandomData(key));
-                }
-            } else {
-                generated = ""
-                for (var i = 0; i < length; i++) {
-                    generated += String.fromCharCode(Math.floor(rand() * 255));
-                }
-            }
-            break
-
-        default:
-            generated = template;
-            break;
-    }
-    return generated;
-
-}
-
-
-function getRandomData(key) {
-    key = key.substr(1); // remove "@"
-    
-    //var params = key.match(/\(((\d+),?)+\)/g) || [];
-    var params = key.match(/\(([^\)]+)\)/g) || [];
-    
-    if (!(key in $.mockJSON.data)) {
-        console.log(key);
-        console.log(params);
-        return key;
-    }
-    
-    var a = $.mockJSON.data[key];
-    
-    switch (type(a)) {
-        case 'array':
-            var index = Math.floor(a.length * rand());
-            return a[index];
-            
-        case 'function':
-            return a();
+function rand() {
+    if ($.mockJSON.random) {
+        return Math.random();
+    } else {
+        _random_numbers = _random_numbers.concat(_random_numbers.splice(0,1));
+        return _random_numbers[0];
     }
 }
-
 
 function type(obj) {
     return $.isArray(obj)
@@ -146,29 +46,80 @@ function pad(num) {
     if (num < 10) {
         return '0' + num;
     }
-    return num + '';
+    return num + String();
 }
-
-var _random_numbers = [0.021768910889510606,0.23762323165420307,0.9079616118204306,0.6534305309997466,0.22049697572443694,0.07687466163364898,0.8017428775547905,0.16165353264404825,0.5124345671670483,0.19337327636624613,0.39963994200698416,0.8012592654139514,0.22474962687229938,0.9791396234452399,0.7965428353317756,0.9777664340629622,0.5135216702983731,0.7407128236192145,0.12880984991420075,0.8186600800491484,0.5187691445438851,0.034723021925916586,0.5625092833040853,0.02502838571997701,0.663696305503698,0.3481608684353138,0.8991623585175106,0.3640542564277087,0.8320766874121723,0.012778915627689846,0.1427680370061336,0.9774408289203956,0.010229381207667587,0.2596610885223093,0.6150540104297127,0.7130773919030915,0.8638338302974085,0.6178483032907357,0.980312844391733,0.5007277415012348,0.6348672031113127,0.4400097775503303,0.8468458451408212,0.38724997893647317,0.690237920987028,0.19850102297146477,0.44895115941315766,0.22283381913760725,0.031228117310125314,0.3367510872581615,0.28155752394210787,0.14696694832580504,0.08164635161760991,0.8837733477785624,0.4590179148539142,0.9613195413217465,0.11263127577456922,0.743695635896287,0.0002424891439143373,0.1964622832546613,0.7333363138878922,0.5575568682003356,0.20426374168098604,0.18030934250338893,0.9792636408392759,0.30121911048336913,0.7734906886720265,0.6984051127767527,0.6638058511379343,0.3310956256388182,0.36632372827973203,0.8996494702333895,0.8235917663049763,0.418496734118911,0.8164648495097332,0.9457831606354686,0.2845227542117049,0.42374718399151545,0.3431728911657228,0.5289314006219973,0.6029243600407113,0.6528301140700757,0.6948768236197832,0.7887302784092911,0.8950274196119906,0.6121642239166305,0.31797481561514696,0.34903732589844216,0.3580320092281766,0.8312225728434115,0.32331010157206974,0.16395388672837796,0.6072960306003872,0.6580526967999424,0.23472961545632742,0.6138637855489343,0.3067303339060682,0.44935935129958315,0.24729465243280668,0.8244189715967711];
-function rand() {
-    if ($.mockJSON.random) {
-        return Math.random();
-    } else {
-        _random_numbers = _random_numbers.concat(_random_numbers.splice(0,1));
-        return _random_numbers[0];
-    }
-}
-
 
 function randomDate() {
     return new Date(Math.floor(rand() * new Date().valueOf()));
 }
 
 
+function getRandomData(key) {
+    var params, a, index, ret;
+
+    key = key.substr(1); // remove "@"
+    
+    params = key.match(/\(([^\)]+)\)/g) || [];
+    
+    // We can now pass a match form the url directly into our JSON.
+    // The prefix to replace is @REGEXP_MATCH_n, where n is an integer
+    // >= 0.  NB: the first grouped match is generally not useful, as
+    // javascript will always return the entire string as the first match.
+    // Group matches start at 1.
+    if ( key.substr(0, 13) === 'REGEXP_MATCH_' ) {
+        var pos = key.substring(13);
+	if ( pos === '' || url_matches[pos] === undefined ) {
+           return key;
+        }
+	return url_matches[pos];
+    } else if ($.mockJSON.data.hasOwnProperty(key)) {
+    
+        a = $.mockJSON.data[key];
+        
+        switch (type(a)) {
+            case 'array':
+                index = Math.floor(a.length * rand());
+                ret = a[index];
+                break;
+                
+            case 'function':
+                ret = a();
+                break;
+        }
+    } else {
+        ret = key;
+    }
+    return ret;
+}
+
+$.mockJSON = function(request, template, wait) {
+    var i, mock;
+
+    if (typeof wait === "undefined") {
+        wait = 0;
+    }
+
+    for (i = 0; i < _mocked.length; i = i + 1) {
+        mock = _mocked[i];
+        if (mock.request.toString() === request.toString()) {
+            _mocked.splice(i, 1);
+            break;
+        }
+    }
+
+    _mocked.push({
+        request:request,
+        template:template,
+        wait: wait
+    });
+    
+    return $;
+};
+
 $.mockJSON.data = {
-    NUMBER : "0123456789".split(''),
-    LETTER_UPPER : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''),
-    LETTER_LOWER : "abcdefghijklmnopqrstuvwxyz".split(''),
+    NUMBER : String.prototype.split.call('0123456789', ''),
+    LETTER_UPPER : String.prototype.split.call('ABCDEFGHIJKLMNOPQRSTUVWXYZ', ''),
+    LETTER_LOWER : String.prototype.split.call('abcdefghijklmnopqrstuvwxyz', ''),
     MALE_FIRST_NAME : ["James", "John", "Robert", "Michael", "William", "David",
         "Richard", "Charles", "Joseph", "Thomas", "Christopher", "Daniel", 
         "Paul", "Mark", "Donald", "George", "Kenneth", "Steven", "Edward",
@@ -194,7 +145,7 @@ $.mockJSON.data = {
     },
     DATE_YYYY : function() {
         var yyyy = randomDate().getFullYear();
-        return yyyy + '';
+        return yyyy + String();
     },
     DATE_DD : function() {
         return pad(randomDate().getDate());
@@ -212,21 +163,136 @@ $.mockJSON.data = {
         return pad(randomDate().getSeconds());
     },
     LOREM : function() {
-        var words = 'lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum'.split(' ');
-        var index = Math.floor(rand() * words.length);
+        var words = String.prototype.split.call('lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum', ' '),
+            index = Math.floor(rand() * words.length);
         return words[index];
     },
     LOREM_IPSUM : function() {
-        var words = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'.split(' ');
-        var result = [];
-        var length = Math.floor(rand() * words.length / 2);
-        for (var i = 0; i < length; i++) {
-            var index = Math.floor(rand() * words.length);
+        var words = String.prototype.split.call('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', ' '),
+            result = [],
+            length = Math.floor(rand() * words.length / 2),
+            i, index;
+        for (i = 0; i < length; i = i + 1) {
+            index = Math.floor(rand() * words.length);
             result.push(words[index]);
         }
         return result.join(' ');
     }
 };
 
+$.mockJSON.random = true;
 
-})(jQuery);
+$.ajax = function(url, options) {
+    var delay = function (options, defer, resp) {
+            options.success(resp);
+            defer.resolve(resp);
+        },
+        i, mock, defer, resp;
+
+    // Mimic jQuery >= 1.5 argument handling
+    if ( typeof url === "object" ) {
+        options = url;
+        url = undefined;
+    }
+
+    options = options || {};
+
+    if (options.dataType === 'json') {
+        for (i = 0; i < _mocked.length; i = i + 1) {
+            mock = _mocked[i];
+            defer = $.Deferred();
+
+            if (mock.request.test(options.url)) {
+                // Store the URL matches for this request
+		url_matches = options.url.match(mock.request);
+                // Store the query string parameters for this request
+		parseQueryString(options.url);
+                resp = $.mockJSON.generateFromTemplate(mock.template);
+                
+                setTimeout(delay, mock.wait, options, defer, resp);
+                
+                defer.success = defer.done;
+                defer.error = defer.fail;
+
+                return defer;
+            }
+        }
+    }
+    return _original_ajax.call(this, url, options);
+};
+
+
+$.mockJSON.generateFromTemplate = function(template, name) {
+    var length = 0, length_min, length_max,
+        matches = (name || '').match(/\w+\|(\d+)-(\d+)/),
+        generated = null,
+        i, p, inc_matches, increment, keys, key;
+    if (matches) {
+        length_min = parseInt(matches[1], 10);
+        length_max = parseInt(matches[2], 10);
+        length = Math.round(rand() * (length_max - length_min)) + length_min;
+    }
+
+    switch (type(template)) {
+        case 'array':
+            generated = [];
+            for (i = 0; i < length; i = i + 1) {
+                generated[i] = $.mockJSON.generateFromTemplate(template[0]);
+            }
+            break;
+
+        case 'object':
+            generated = {};
+            for (p in template) {
+                if (template.hasOwnProperty(p)) {
+                    generated[p.replace(/\|(\d+-\d+|\+\d+)/, '')] = $.mockJSON.generateFromTemplate(template[p], p);
+                    inc_matches = p.match(/\w+\|\+(\d+)/);
+                    if (inc_matches && type(template[p]) === 'number') {
+                        increment = parseInt(inc_matches[1], 10);
+                        template[p] += increment;
+                    }
+                }
+            }
+            break;
+
+        case 'number':
+            generated = (matches)
+                ? length
+                : template;
+            break;
+
+        case 'boolean':
+            generated = (matches)
+                ? rand() >= 0.5
+                : template;
+            break;
+
+        case 'string':
+            if (template.length) {
+                generated = '';
+                length = length || 1;
+                for (i = 0; i < length; i = i + 1) {
+                    generated += template;
+                }
+                keys = generated.match(/@([A-Z_0-9\(\),]+)/g) || [];
+                for (i = 0; i < keys.length; i = i + 1) {
+                    key = keys[i];
+                    generated = generated.replace(key, getRandomData(key));
+                }
+            } else {
+                generated = "";
+                for (i = 0; i < length; i = i + 1) {
+                    generated += String.fromCharCode(Math.floor(rand() * 255));
+                }
+            }
+            break;
+
+        default:
+            generated = template;
+            break;
+    }
+    return generated;
+
+};
+
+}(jQuery));
